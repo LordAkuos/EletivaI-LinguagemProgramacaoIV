95d78088c0e819e75bff819ea8bc383093d18d73
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-03-16 20:16:13 -0300

    Update resultado.php

--- Aula1603/Lista2/Exer1/resultado.php
+++ Aula1603/Lista2/Exer1/resultado.php
@@ -30,7 +30,8 @@
                     $posicao = $chave;
                 }   
             }
-            echo "O maior valor é $maior e está na posição $chave";
+            echo "<br>";
+            echo "O maior valor é $maior e está na posição $posicao";
 
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
