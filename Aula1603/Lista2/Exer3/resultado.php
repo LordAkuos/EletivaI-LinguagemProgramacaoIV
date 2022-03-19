27677d0f0b5b53bc8b4890fea874b611e9c70e7e
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-03-18 22:05:09 -0300

    Update resultado.php

--- Aula1603/Lista2/Exer3/resultado.php
+++ Aula1603/Lista2/Exer3/resultado.php
@@ -19,8 +19,11 @@
             } 
             foreach($vetor as $chave => $valor) {
                 echo "O valor $valor está na posição $chave";
-                echo "<br>";
+                echo "<br>"; 
             }
+            /*if ($valor == $valor) {
+                    echo "-";
+            }*/
 
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
