593217a3b86acec6a8fb91fe370dadb4b32a10ac
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-03-18 21:52:20 -0300

    Update index.php

--- Aula1603/Lista2/Exer3/index.php
+++ Aula1603/Lista2/Exer3/index.php
@@ -14,7 +14,24 @@
         <h1>Exercício 03</h1>
         <br>
         <form action="resultado.php" method="POST">
-                  
+            <div class="row">
+                <?php
+                    for ($i=1; $i <= 10; $i++) { 
+                ?>
+                    <div class="col">
+                        <label for="valor<?= $i ?>">Informe o valor<?= $i ?></label>
+                        <input type="number" class="form-control" name="valor<?= $i ?>" id="valor<?= $i ?>">
+                    </div>
+                <?php
+                    }
+                ?>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col">
+                    <button type="submit" class="btn btn-secondary">Enviar</button>
+                </div>
+            </div>   
         </form>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <!-- Option 2: Separate Popper and Bootstrap JS -->
