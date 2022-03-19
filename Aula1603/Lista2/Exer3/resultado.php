9cca2148887a6cfed21967928526f072798c3f8e
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-03-18 21:58:48 -0300

    Update resultado.php

--- Aula1603/Lista2/Exer3/resultado.php
+++ Aula1603/Lista2/Exer3/resultado.php
@@ -14,7 +14,14 @@
         <h1>Resposta do exercício 03</h1>
         <br>
         <?php
-            
+            for ($i=1; $i <= 10; $i++) { 
+                $vetor[$i] = $_POST["valor$i"];
+            } 
+            foreach($vetor as $chave => $valor) {
+                echo "O valor $valor está na posição $chave";
+                echo "<br>";
+            }
+
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <!-- Option 2: Separate Popper and Bootstrap JS -->
