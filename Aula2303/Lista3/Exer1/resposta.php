2038ea15d38f2d0dd66752e8503a21b8cd62a390
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-03-23 20:12:00 -0300

    Update resposta.php

--- Aula2303/Lista3/Exer1/resposta.php
+++ Aula2303/Lista3/Exer1/resposta.php
@@ -13,7 +13,17 @@
     <body class="container">
         <h1>Resposta do exercício 01</h1>
         <br/>
+        <?php
+            function verificarData($dia, $mes, $ano){
+                if (checkDate($mes, $dia, $ano)) {
+                    echo "Essa é uma data válida!";
+                }else {
+                    echo "Essa não é uma data válida.";
+                }
+            }
         
+        ?>
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <!-- Option 2: Separate Popper and Bootstrap JS -->
         <!--
