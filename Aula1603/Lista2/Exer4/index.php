fa7cbf7f329f07c8b78b5dc33c3de4c0eededa58
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-03-23 20:22:44 -0300

    Update index.php

--- Aula1603/Lista2/Exer4/index.php
+++ Aula1603/Lista2/Exer4/index.php
@@ -26,6 +26,11 @@
                     }
                 ?>
             </div>
+            <div class="row">
+                <div class="col">
+                    <button type="submit" class="btn btn-primary">Calcular média</button>
+                </div>
+            </div>  
         </form>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <!-- Option 2: Separate Popper and Bootstrap JS -->
