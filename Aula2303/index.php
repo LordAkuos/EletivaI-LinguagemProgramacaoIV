ccdb70eed2c461ea5d5ab16c0ced7dcaabf73967
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-03-23 19:34:15 -0300

    Update index.php

--- Aula2303/index.php
+++ Aula2303/index.php
@@ -16,6 +16,11 @@
         <?= date("d/m/Y"); ?>
         <br>
         <?= date("h:i"); ?>
+        <br>
+        <?php
+            $valor = 100004.789;
+            echo "R$ ".(number_format($valor,1,',','.'));
+        ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <!-- Option 2: Separate Popper and Bootstrap JS -->
         <!--
