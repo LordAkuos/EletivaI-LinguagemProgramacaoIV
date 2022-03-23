d13a1bd5dbe00db85cbf147c7c01093a1f48234a
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-03-23 20:41:06 -0300

    Update resposta.php

--- Aula1603/Lista2/Exer5/resposta.php
+++ Aula1603/Lista2/Exer5/resposta.php
@@ -13,7 +13,11 @@
     <body class="container">
         <h1>Resposta do exercício 05</h1>
         <br/>
-        
+        <?php
+            for ($i=1; $i <= 10; $i++){
+                $vetor[$i] = $_POST["valor$i"];
+            }        
+        ?>        
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <!-- Option 2: Separate Popper and Bootstrap JS -->
