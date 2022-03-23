a0afac54cf598f749162647cb4ae159a9ada089d
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-03-23 20:14:32 -0300

    Update resposta.php

--- Aula2303/Lista3/Exer1/resposta.php
+++ Aula2303/Lista3/Exer1/resposta.php
@@ -21,7 +21,11 @@
                     echo "Essa não é uma data válida.";
                 }
             }
-        
+            $diaInformado = $_POST['dia'];
+            $mesInformado = $_POST['mes'];
+            $anoInformado = $_POST['ano'];
+
+            verificarData($diaInformado, $mesInformado, $anoInformado);
         ?>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
