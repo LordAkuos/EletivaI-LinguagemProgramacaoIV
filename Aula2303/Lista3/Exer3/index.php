a568eb6a512ce2fc39e19118a3ea44f1cb724ad5
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-03-25 22:12:27 -0300

    Update index.php

--- Aula2303/Lista3/Exer3/index.php
+++ Aula2303/Lista3/Exer3/index.php
@@ -13,7 +13,23 @@
     <body class="container">
         <h1>Exercício 03</h1>
         <br/>
-        
+        <form action="" method="POST">
+            <div class="row">
+                <div class="col">
+                    <label for="numero1" class="label-control">1º número:</label>
+                    <input type="number" class="form-control" id="numero1">
+                </div>
+                <div class="col">
+                    <label for="numero2" class="label-control">1º número:</label>
+                    <input type="number" class="form-control" id="numero2">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <button type="submit" class="btn btn-sucess">Enviar</button>
+                </div>
+            </div>
+        </form>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <!-- Option 2: Separate Popper and Bootstrap JS -->
         <!--
