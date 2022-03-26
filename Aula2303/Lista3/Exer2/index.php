645de3bd7ada873e280b6716b009a63210e46c8b
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-03-25 21:17:20 -0300

    Aula2503

--- Aula2303/Lista3/Exer2/index.php
+++ Aula2303/Lista3/Exer2/index.php
@@ -8,29 +8,29 @@
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-        <title>Lista de exercícios 2</title>
+        <title>Lista de exercícios 3</title>
     </head>
     <body class="container">
-        <h1>Exercício 05</h1>
+        <h1>Exercício 01</h1>
         <br/>
         <form action="resposta.php" method="POST">
             <div class="row">
-            <?php
-                for ($i=1; $i <= 10; $i++) { 
-            ?>
-                <div class="col">
-                    <label for="valor<?= $i ?>">Informe o valor <?= $i ?>:</label>
-                    <input type="number" class="form-control" name="valor<?= $i ?>" id="valor<?= $i ?>">
-                </div>
-            <?php
-                }
-            ?>
+                <?php
+                    for ($i=1; $i <= 10; $i++) { 
+                ?>
+                    <div class="col">
+                        <label for="nome<?= $i ?>"><?= $i ?> Informe o nome:</label>
+                        <input type="text" class="form-control" name="nome<?= $i ?>" id="nome<?= $i ?>">
+                    </div>
+                <?php
+                    }
+                ?>
             </div>
             <div class="row">
                 <div class="col">
-                    <button type="submit" class="btn btn-success">Enviar</button>
+                    <button type="submit" class="btn btn-warning">Enviar</button>
                 </div>
-            </div>  
+            </div>
         </form>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <!-- Option 2: Separate Popper and Bootstrap JS -->
