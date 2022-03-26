c7abcd4740c0ba3e56f67ad1fb223aba1fd20c2b
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-03-25 21:43:49 -0300

    Update resposta.php

--- Aula1603/Lista2/Exer5/resposta.php
+++ Aula1603/Lista2/Exer5/resposta.php
@@ -16,7 +16,13 @@
         <?php
             for ($i=1; $i <= 10; $i++){
                 $vetor[$i] = $_POST["valor$i"];
-            }        
+            }
+            sort($vetor);
+            print_r($vetor);
+            echo "<br>";
+            rsort($vetor);
+            print_r($vetor);
+                    
         ?>        
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
