e54212d9305753ef22c3ddfc239f18481ed2badd
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-03-30 19:43:43 -0300

    Aula3003

--- ListaExercicios1/Exer4/index.php
+++ ListaExercicios1/Exer4/index.php
@@ -8,34 +8,22 @@
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-        <title>Sistema em PHP</title>
+        <title>Exercício 04</title>
     </head>
     <body class="container">
-        <h1>Sistema em PHP</h1>
+        <h1>Verificar o número</h1>
         <br>
         <form action="resultado.php" method="POST">
             <div class="row">
-                <div class="col"></div>
-                <div class="col">
-                    <div class="modal" tabindex="-1">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title">Modal title</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <p>Modal body text goes here.</p>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Save changes</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <div class="col-6">
+                    <label for="num" class="label-control">Informe um número</label>
+                    <input type="number" id="num" name="num" class="form-control">
                 </div>
-                <div class="col"></div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <button type="submit" class="btn btn-danger">Enviar</button>
+                </div>
             </div>
         </form>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
