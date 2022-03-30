3142d0f95c70678ca6af871dc5b97adb04cb845b
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-03-30 20:01:30 -0300

    Aula3003

--- ListaExercicios1/Exer5/index.php
+++ ListaExercicios1/Exer5/index.php
@@ -8,12 +8,19 @@
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-        <title>Sistema em PHP</title>
+        <title>Exercício 05</title>
     </head>
     <body class="container">
-        <h1>Calculadora de troco</h1>
+        <h1>Cálculo da média</h1>
         <br>
-        
+        <form action="resultado.php" method="POST">
+            
+            <div class="row">
+                <div class="col">
+                    <button type="submit" class="btn btn-warning">Calcular</button>
+                </div>
+            </div>
+        </form>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <!-- Option 2: Separate Popper and Bootstrap JS -->
         <!--
--- ListaExercicios1/Exer5/resultado.php
+++ ListaExercicios1/Exer5/resultado.php
@@ -8,10 +8,10 @@
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-        <title>Sistema em PHP</title>
+        <title>Exercício 05</title>
     </head>
     <body class="container">
-        <h1>Calculadora de troco</h1>
+        <h1>Resposta do exercício 05</h1>
         <br>
         
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
