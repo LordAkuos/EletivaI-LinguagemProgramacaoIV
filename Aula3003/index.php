a57ecd454a18425f3efcb0a9a0c0ffad5475c92b
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-03-30 19:37:04 -0300

    Aula3003

--- Aula3003/index.php
+++ Aula3003/index.php
@@ -14,7 +14,11 @@
         <h1>Sistema em PHP</h1>
         <br>
         <form action="resultado.php" method="POST">
-            
+            <div class="row">
+                <div class="col"></div>
+                <div class="col"></div>
+                <div class="col"></div>
+            </div>
         </form>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <!-- Option 2: Separate Popper and Bootstrap JS -->
--- ListaExercicios1/Exer3/resultado.php
+++ ListaExercicios1/Exer3/resultado.php
@@ -15,6 +15,15 @@
         <br>
         <?php
             $num = $_POST['numero'];
+            if ($num < 10) {
+                echo "O número $num é menor que 10.";
+            }
+            if ($num == 10) {
+                echo $num;
+            }
+            if ($num > 10) {
+                echo "O número $num é maior que 10.";
+            }
         
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
