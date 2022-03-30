abbbd9ed42092a7d042ccc9b0d64b27c8f5b36a0
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-03-30 20:14:16 -0300

    Update resultado.php

--- ListaExercicios1/Exer5/resultado.php
+++ ListaExercicios1/Exer5/resultado.php
@@ -14,9 +14,13 @@
         <h1>Resposta do exercício 05</h1>
         <br>
         <?php
+            $soma = 0;
             for ($i=1; $i <= 4; $i++) { 
                 $vetor[$i] = $_POST["valor$i"];
+                $soma = $soma + $vetor[$i];
             }
+            $media = $soma / $vetor[$i];
+            echo "A média do aluno é $media";
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <!-- Option 2: Separate Popper and Bootstrap JS -->
