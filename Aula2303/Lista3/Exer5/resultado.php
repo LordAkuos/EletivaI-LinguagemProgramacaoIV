4f0a3bc61f256a7b122030770497bee6f705e600
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-04-06 20:03:12 -0300

    Update resultado.php

--- Aula2303/Lista3/Exer5/resultado.php
+++ Aula2303/Lista3/Exer5/resultado.php
@@ -13,6 +13,37 @@
     <body class="container">
         <h1>Resposta do exercício 05</h1>
         <br/>
+        <?php
+
+            function calcularIMC($peso, $altura){
+                $imc = $peso / $altura;
+                if ($imc <= 18.5) {
+                    echo "IMC: ".$imc." Abaixo do peso.";
+                }
+                if ($imc >= 18.6 && $imc <= 24.9){
+                    echo "IMC: ".$imc." Peso ideal (Parabéns!).";
+                }
+                if ($imc >= 25 && $imc <= 29.9){
+                    echo "IMC: ".$imc." Levemente acima do peso.";
+                }
+                if ($imc >= 30 && $imc <= 34.9) {
+                    echo "IMC: ".$imc." Obesidade grau I.";
+                }
+                if ($imc >= 35 && $imc <= 39.9) {
+                    echo "IMC: ".$imc." Obesidade grau II (severa).";
+                }
+                if ($imc >= 40) {
+                    echo "IMC: ".$imc." Obesidade grau III (mórbida).";
+                }
+                return $imc;
+            }
+
+            $peso = $_POST['peso'];
+            $altura = $_POST['altura'];
+            echo calcularIMC($peso, $altura);
+
+        
+        ?>
         
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <!-- Option 2: Separate Popper and Bootstrap JS -->
