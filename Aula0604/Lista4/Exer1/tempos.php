08ff547d30f330efe8912d2e3ceba175913e059b
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-04-06 20:27:37 -0300

    Aula0604

--- Aula0604/Lista4/Exer1/tempos.php
+++ Aula0604/Lista4/Exer1/tempos.php
@@ -11,9 +11,19 @@
         <title>Lista de exercícios 4</title>
     </head>
     <body class="container">
-        <h1>Resposta do exercício 01</h1>
+        <h1>Exercício 01 - Cadastrar tempos</h1>
         <br/>
+        <?php
+
+            $voltas = $_POST['voltas'];
+            
         
+        ?>
+        <form action="resposta.php" method="POST">
+            <div class="row">
+                <button type="submit" class="btn btn-warning">Cadastrar tempos</button>
+            </div>
+        </form>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <!-- Option 2: Separate Popper and Bootstrap JS -->
         <!--
