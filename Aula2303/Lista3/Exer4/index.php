a63ceffd30256364c9cb6507edd86d968c297bfa
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-04-06 19:41:14 -0300

    Update index.php

--- Aula2303/Lista3/Exer4/index.php
+++ Aula2303/Lista3/Exer4/index.php
@@ -18,8 +18,8 @@
                 for ($i=0; $i <= 5; $i++) { 
             ?>
                 <div class="col">
-                    <label for="numero<?= $i ?>"><?= $i ?> Informe o <?= $i ?>º número:</label>
-                    <input type="text" class="form-control" name="numero<?= $i ?>" id="numero<?= $i ?>">
+                    <label for="numero"><?= $i ?> Informe o <?= $i ?>º número:</label>
+                    <input type="text" class="form-control" name="numero" id="numero">
                 </div>
             <?php
                 }
