6453355f5e5976ce16eac82ae8584211d46c2c85
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-04-08 21:54:09 -0300

    Update cheque.php

--- Aula0604/Lista4/Exer2/cheque.php
+++ Aula0604/Lista4/Exer2/cheque.php
@@ -27,15 +27,15 @@
                     <input type="float" id="valor_cheque" name="valor_cheque" class="form-control">
                 </div>
             </div>
-            <div class="row">
-                <div class="col">
-                    <button type="submit" class="btn btn-primary">Somar</button>
-                </div>
-            </div>
         </form>
         <?php
             }
         ?>
+        <div class="row">
+            <div class="col">
+                <button type="submit" class="btn btn-primary">Somar</button>
+            </div>
+        </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <!-- Option 2: Separate Popper and Bootstrap JS -->
         <!--
