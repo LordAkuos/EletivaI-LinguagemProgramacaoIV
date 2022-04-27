4c1de74f2da9b0dc4826b4a62e18e44901ac5f3a
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-04-27 20:30:57 -0300

    Update Laser.php

--- Aula2704/Laser.php
+++ Aula2704/Laser.php
@@ -2,7 +2,22 @@
 
 class Laser extends Impressora
 {
-    
+    private $capacidadeToner;
+    private $frenteVerso;
+
+    public function getCapacidadeToner(){
+        return $this->capacidadeToner;
+    }
+    public function setCapacidadeToner($capacidadeToner){
+        $this->capacidadeToner = $capacidadeToner;
+    }
+
+    public function getFrenteVerso(){
+        return $this->frenteVerso;
+    }
+    public function setFrenteVerso($frenteVerso){
+        $this->frenteVerso = $frenteVerso;
+    }
 }
 
 
