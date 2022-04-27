25fe7b3af1be9777f8888d6f110de833475c400d
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-04-27 20:10:04 -0300

    Update Impressora.php

--- Aula2704/Impressora.php
+++ Aula2704/Impressora.php
@@ -6,7 +6,36 @@ class Impressora {
     private $colorida;
     private $peso;
 
-    
+    public function getMarca(){
+        return $this->marca;
+    }
+    public function setMarca($marca){
+        $this->marca = $marca;
+    }
+
+    public function getModelo(){
+        return $this->modelo;
+    }
+    public function setModelo($modelo){
+        $this->modelo = $modelo;
+    }
+
+    public function getColorida(){
+        return $this->colorida;
+    }
+    public function setColorida($colorida){
+        $this->colorida = $colorida;
+    }
+
+    public function getPeso(){
+        return $this->peso;
+    }
+    public function setPeso($peso){
+        $this->peso = $peso;
+    }
+
+
+
 
 }
 
