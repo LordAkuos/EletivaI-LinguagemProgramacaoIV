31841eba24fcd5c891341a8f09071be3ec46c729
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-04-27 20:22:58 -0300

    Update Matricial.php

--- Aula2704/Matricial.php
+++ Aula2704/Matricial.php
@@ -5,9 +5,9 @@ class Matricial extends Impressora {
     private $numAgulhas;
     private $imprimeVias;
 
-    public function __construct()
+    public function __construct($marca, $modelo, $numAgulhas, $colorida, $imprimeVias, $peso)
     {
-        
+        $this->marca = $marca;
     }
 
     public function getNumAgulhas(){
