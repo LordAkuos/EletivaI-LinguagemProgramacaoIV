2c227b71d78e807f440998e6505797fc95b9885f
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-04-27 20:35:38 -0300

    Update index.php

--- Aula2704/index.php
+++ Aula2704/index.php
@@ -8,7 +8,7 @@
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-        <title>Programação Orientada a Objeto em PHP</title>
+        <title>Orientação a Objetos em PHP</title>
     </head>
     <body class="container">
         <br/>
