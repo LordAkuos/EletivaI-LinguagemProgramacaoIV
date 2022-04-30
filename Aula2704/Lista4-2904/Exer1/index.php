04a7767bd2bbb1a781e5bd1bb4c5eac689a62f1f
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-04-29 22:02:22 -0300

    Update index.php

--- Aula2704/Lista4-2904/Exer1/index.php
+++ Aula2704/Lista4-2904/Exer1/index.php
@@ -5,6 +5,6 @@
     echo "Quantidade de objetos criados: ".Ponto::getContador();
 
     $p2 = new Ponto(4,6);
-    echo "Distância entre os pontos: ".$p1->calcularDistancia($p);
+    echo "Distância entre os pontos: ".$p1->calcularDistancia($p2);
 
 ?>
