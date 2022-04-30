b2efb3dc26f35df15ed1f3d4822e200cdb8c2913
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-04-29 21:15:18 -0300

    Create Ponto.php

---
+++ Aula2704/Lista4-2904/Exer1/Ponto.php
@@ -0,0 +1,24 @@
+<?php
+
+class Ponto {
+
+    private $x;
+    private $y;
+
+    public function getX(){
+        return $this->x;
+    }
+    public function setX($x){
+        $this->x = $x;
+    }
+
+    public function getY(){
+        return $this->y;
+    }
+    public function setY($y){
+        $this->y = $y;
+    }
+
+}
+
+?>
