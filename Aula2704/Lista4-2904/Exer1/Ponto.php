e2f90580cd21a69f58b563fd7110be9f041dbf9c
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-04-29 21:19:13 -0300

    Update Ponto.php

--- Aula2704/Lista4-2904/Exer1/Ponto.php
+++ Aula2704/Lista4-2904/Exer1/Ponto.php
@@ -5,6 +5,12 @@ class Ponto {
     private $x;
     private $y;
 
+    public function __construct($x, $y)
+    {
+        $this->setX($x);
+        $this->setY($y);
+    }
+
     public function getX(){
         return $this->x;
     }
