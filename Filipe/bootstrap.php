f022ccda227a181507cad75ce7a0cd79f6a93fc6
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-05-04 20:26:58 -0300

    Update bootstrap.php

--- Filipe/bootstrap.php
+++ Filipe/bootstrap.php
@@ -4,3 +4,4 @@ require_once __DIR__.'/vendor/autoload.php';
 
 //Aqui, a gente recupera o que o usuário digitou e qual método HTTP ele utilizou.
 $method = $_SERVER['REQUEST_METHOD'];
+$path = $_SERVER['PATH_INFO'];
