f7f1a5ae5c10a80af917848bbaca93ca9621c66e
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-05-04 20:28:50 -0300

    Update bootstrap.php

--- Filipe/bootstrap.php
+++ Filipe/bootstrap.php
@@ -5,3 +5,6 @@ require_once __DIR__.'/vendor/autoload.php';
 //Aqui, a gente recupera o que o usuário digitou e qual método HTTP ele utilizou.
 $method = $_SERVER['REQUEST_METHOD'];
 $path = $_SERVER['PATH_INFO'];
+
+//INstanciar classe Router:
+$router = new \Aluno\Filipe\Router($method, $path);
