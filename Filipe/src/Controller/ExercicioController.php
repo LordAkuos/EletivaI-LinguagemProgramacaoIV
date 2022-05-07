eed9178651370497c073d7f2fc566aa2932f6c2b
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-05-06 22:02:49 -0300

    Update ExercicioController.php

--- Filipe/src/Controller/ExercicioController.php
+++ Filipe/src/Controller/ExercicioController.php
@@ -14,5 +14,6 @@ class ExercicioController
         $valor1 = $_POST['valor1'];
         $valor2 = $_POST['valor2'];
         $soma = $valor1 + $valor2;
+        require_once("../src/View/exemplo.php");
     }
 }
