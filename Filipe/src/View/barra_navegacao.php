cfdc9bafae2fbc118a313eee2d279960cc6c383f
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-05-25 20:02:35 -0300

    Update barra_navegacao.php

--- Filipe/src/View/barra_navegacao.php
+++ Filipe/src/View/barra_navegacao.php
@@ -7,8 +7,8 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">
-              Home
+            <a class="nav-link active" aria-current="page" href="/clientes">
+              Clientes
             </a>
         </li>
       </ul>
