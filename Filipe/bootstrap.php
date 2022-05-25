564f10b722630b55c8517c80376c5ce2fd6af2c8
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-05-25 19:43:05 -0300

    Aula2505

--- Filipe/bootstrap.php
+++ Filipe/bootstrap.php
@@ -2,28 +2,37 @@
 
 require_once __DIR__.'/vendor/autoload.php';
 
-//Aqui, a gente recupera o que o usuário digitou e qual método HTTP ele utilizou.
+//Aqui a gente recupera o que o usuário digitou e qual 
+//método HTTP ele utilizou
 $method = $_SERVER['REQUEST_METHOD'];
 $path = $_SERVER['PATH_INFO'];
 
-//Instanciar classe Router:
-$router = new \Aluno\Filipe\Router($method, $path);
+//Instanciar classe Router
+$router = new \Aluno\ProjetoPhp\Router($method, $path);
 
-//Adicionar as rotas válidas abaixo:
+//ADICIONAR AS ROTAS VÁLIDAS ABAIXO
 
 $router->get('/ola-mundo', function(){
     return "Olá Mundo!";
 });
 
-$router->get('/exemplo', 'Aluno\Filipe\Controller\ExercicioController::exibir');
+$router->get('/exemplo', 
+        'Aluno\ProjetoPhp\Controller\ExercicioController::exibir');
 
-$router->post('/exemplo-resultado', 'Aluno\Filipe\Controller\ExercicioController::exibirResultado');
+$router->post('/exemplo-resultado',
+        'Aluno\ProjetoPhp\Controller\ExercicioController::exibirResultado');
 
-//Adicionar as rotas válidas acima
+$router->get('/cliente/novo',
+'Aluno\ProjetoPhp\Controller\ClientesController::abrirFormularioInserir');
+
+$router->post('/cliente/inserir',
+'Aluno\ProjetoPhp\Controller\ClientesController::inserirCliente');
+
+//ADICIONAR AS ROTAS VÁLIDAS ACIMA
 
 $result = $router->handler();
 
-if (!$result) {
+if (!$result){
     http_response_code(404);
     echo "Página não encontrada";
     die();
