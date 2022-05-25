665014078c89d3c67dabcc54b1582cd3584c2822
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-05-25 20:01:49 -0300

    Update listar_clientes.php

--- Filipe/src/View/listar_clientes.php
+++ Filipe/src/View/listar_clientes.php
@@ -12,9 +12,7 @@
   </head>
   <body class="container">
     <h1>Inserir Cliente</h1>
-
     
-
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
