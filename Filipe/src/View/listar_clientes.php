f0700a8dfcc8eb1d1cbbe12524adfaba43a815df
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-05-27 21:15:45 -0300

    Update listar_clientes.php

--- Filipe/src/View/listar_clientes.php
+++ Filipe/src/View/listar_clientes.php
@@ -41,23 +41,14 @@
             </tr>
         </thead>
         <tbody>
-            <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            </tr>
-            <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-            </tr>
-            <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-            </tr>
+            <?php while($linha = $resultado->fetch(PDO::FETCH_ASSOC)){ ?>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+            <?php } ?> 
         </tbody>
     </table>
     <!-- Optional JavaScript; choose one of the two! -->
