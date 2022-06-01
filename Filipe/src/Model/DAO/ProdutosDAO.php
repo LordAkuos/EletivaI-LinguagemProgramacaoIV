5df419a8039215b5bf7c7a2a87707fd57a36ceb4
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-06-01 19:57:19 -0300

    Update ProdutosDAO.php

--- Filipe/src/Model/DAO/ProdutosDAO.php
+++ Filipe/src/Model/DAO/ProdutosDAO.php
@@ -33,11 +33,35 @@ class ProdutosDAO {
     }
 
     public function consultar(){
+        try{
+            $sql = "SELECT * FROM produtos";
+            return Conexao::conectar()->query($sql);
+        } catch(\Exception $e){
+            return false;
+        }
+    }
 
+    public function consultarPorId($id){
+        try{
+            $sql = "SELECT * FROM produtos WHERE id = :id";
+            $p = Conexao::conectar()->prepare($sql);
+            $p->bindValue(":id", $id);
+            $p->execute();
+            return $p->fetch();
+        } catch(\Exception $e){
+            return false;
+        }
     }
 
     public function excluir($id){
-
+        try{
+            $sql = "DELETE FROM `produtos` WHERE id = :id";
+            $p = Conexao::conectar()->prepare($sql);
+            $p->bindValue(":id", $id);
+            return $p->execute();
+        } catch(\Exception $e){
+            return false;
+        }
     }
 }
 
