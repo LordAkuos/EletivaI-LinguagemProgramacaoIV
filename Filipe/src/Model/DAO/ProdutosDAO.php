5ee43791075d2b593a5f865d111ddd0ac91cc245
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-06-01 19:26:52 -0300

    Aula01-06

---
+++ Filipe/src/Model/DAO/ProdutosDAO.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Aluno\ProjetoPhp\Model\DAO;
+
+use Aluno\ProjetoPhp\Model\Entity\Produtos;
+
+class PRodutosDAO {
+    public function inserir(Produtos $p){
+        
+    }
+}
+
+?>
