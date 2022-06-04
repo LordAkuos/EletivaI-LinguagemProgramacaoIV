d031943ac91d822c263552a8d1c263a701b9222a
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-06-03 21:42:55 -0300

    Aula03-06

---
+++ Lista1/Exer1Composer/src/Controller/ExercicioController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Aluno\Lista1\Exer1Composer\Controller;
+
+
+class ExercicioController
+{
+    public static function exibir(){
+        require_once("../src/View/exemplo.php");
+    }
+
+    public static function exibirResultado(){
+        $valor1 = $_POST['valorproduto'];
+        $valor2 = $_POST['valorpago'];
+
+        if ($valor2 >= $valor1) {
+            $troco = $valor2 - $valor1;
+            echo "Seu troco é: R$ $troco</br>";
+        }else {
+            echo "Valor insuficiente!</br>";
+        }
+        require_once("../src/View/exer1.php");
+    }
+}
