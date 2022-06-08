fe9cd6b8129f20cfe34343ef3eaabf2ac5e0aef2
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-06-08 19:24:02 -0300

    Update exer4.php

--- Lista1/Exer4/src/Controller/View/exer4.php
+++ Lista1/Exer4/src/Controller/View/exer4.php
@@ -29,8 +29,17 @@
         <br>
         <?php
         
-        
-        
+        if (isset($numero)) {
+            $numero = $_POST['num'];
+            if ($numero < 0) {
+                echo "Número negativo";
+            }elseif ($numero > 0) {
+                echo "Número positivo";
+            }else {
+                echo "Igual a zero";
+            }
+        }
+
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <!-- Option 2: Separate Popper and Bootstrap JS -->
