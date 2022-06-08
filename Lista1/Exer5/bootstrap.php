53902590443bb42bb01f78274483a914f6d3e5f4
LordAkuos <99516082+LordAkuos@users.noreply.github.com>
2022-06-08 19:46:50 -0300

    Aula08-06

--- Lista1/Exer5/bootstrap.php
+++ Lista1/Exer5/bootstrap.php
@@ -7,7 +7,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 $path = $_SERVER['PATH_INFO'];
 
 //Instanciar classe Router:
-$router = new Aluno\Exer4\Router($method, $path);
+$router = new Aluno\Exer5\Router($method, $path);
 
 //Adicionar as rotas válidas abaixo:
 
